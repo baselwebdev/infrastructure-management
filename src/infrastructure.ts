0f01260906b047da1e2fd4c56ddacd609259962b
Basel <basel@gmx.co.uk>
2021-02-02 22:04:03 +0000

    LP-140 Start infra logic from scratch using AWS JS SDV V3

--- src/infrastructure.ts
+++ src/infrastructure.ts
@@ -1,130 +1,112 @@
-import type { CreateStackInput, DeleteStackInput } from 'aws-sdk/clients/cloudformation';
-import type { AWSError } from 'aws-sdk/lib/error';
-import Authenticator from './authenticator';
-import CloudFormation from 'aws-sdk/clients/cloudformation';
-import { readFileSync } from './fileManager';
-
-interface WaitForParams {
-    StackName: string;
-}
+import type { DescribeStackEventsInput } from '@aws-sdk/client-cloudformation/models/models_0';
+import Client from './client';
+import Config from './config';
+import type { CloudFormationClient } from '@aws-sdk/client-cloudformation';
+import { DescribeStacksCommand } from '@aws-sdk/client-cloudformation';
+import type { BaseError } from './exceptions/baseException';
+import { StackCreationException } from './exceptions/stackCreationException';
+import { StackNotFoundException } from './exceptions/stackNotFoundException';
+import getCurrentLine from 'get-current-line';
+import { StackStatusRetrievingException } from './exceptions/stackStatusRetrievingException';
+import { ErrorCollection } from './exceptions/errorCollection';
 
 export default class Infrastructure {
-    protected Cloud: CloudFormation;
-    protected resourceDirectory: string;
+    protected cloudformationClient: CloudFormationClient;
     protected stackName: string;
+    protected errorCollection: ErrorCollection;
 
     constructor(resourceDirectory: string, stackName: string) {
-        this.resourceDirectory = resourceDirectory;
+        const config = new Config(resourceDirectory);
+
+        this.cloudformationClient = Client.createCloudFormationConnection(config);
         this.stackName = stackName;
-        Authenticator.authenticateAws(resourceDirectory);
-        this.Cloud = new CloudFormation();
+        this.errorCollection = new ErrorCollection();
     }
 
-    protected createStackInput(): CreateStackInput {
-        const cloudFormationStack = readFileSync(
-            this.resourceDirectory + '/CloudFormationStack.json',
-        );
-
-        return {
-            StackName: this.stackName,
-            TemplateBody: JSON.stringify(cloudFormationStack),
-            OnFailure: 'ROLLBACK',
-        };
+    public getErrors(): BaseError[] {
+        return this.errorCollection.get();
     }
 
-    protected deleteStackInput(): DeleteStackInput {
-        return {
+    protected async getStackStatus(): Promise<string> {
+        const params: DescribeStackEventsInput = {
             StackName: this.stackName,
         };
-    }
+        const command = new DescribeStacksCommand(params);
 
-    protected waitForStackExistsInput(): WaitForParams {
-        return {
-            StackName: this.stackName,
-        };
-    }
+        try {
+            const data = await this.cloudformationClient.send(command).catch((error: Error) => {
+                if (error.name === 'ValidationError') {
+                    const stackNotFound = new StackNotFoundException(
+                        this.stackName,
+                        'Stack name was not found in your given AWS account',
+                        `Error thrown in file ${getCurrentLine().file} on line ${
+                            getCurrentLine().line
+                        }`,
+                    );
 
-    public create(): void {
-        const cloudFormationParameters = this.createStackInput();
-        const cloudFormationRequest = this.Cloud.createStack(cloudFormationParameters);
+                    this.errorCollection.add(stackNotFound);
 
-        cloudFormationRequest.on('complete', function (response) {
-            if (response.httpResponse.statusCode === 200) {
-                console.log('Creating infrastructure');
-            } else {
-                console.log('Something went wrong');
-                console.log(response);
-            }
-        });
-        cloudFormationRequest.send();
-    }
-
-    public delete(): void {
-        const cloudFormationParameters = this.deleteStackInput();
-        const cloudFormationRequest = this.Cloud.deleteStack(cloudFormationParameters);
-
-        cloudFormationRequest.on('complete', function (response) {
-            if (response.httpResponse.statusCode === 200) {
-                console.log('Deletion started');
-            } else {
-                console.log('Something went wrong');
-                console.log(response);
-            }
-        });
-        cloudFormationRequest.send();
-    }
-
-    public redeploy(): void {
-        const cloudFormationStackExistParameters = this.waitForStackExistsInput();
-
-        this.Cloud.waitFor(
-            'stackExists',
-            cloudFormationStackExistParameters,
-            (err: AWSError | null, data: CloudFormation.Types.DescribeStacksOutput) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    if (data.Stacks?.length === 1) {
-                        if (data.Stacks[0].StackStatus === 'CREATE_COMPLETE') {
-                            console.log('Existing infrastructure found');
-                            this.delete();
-                            this.Cloud.waitFor(
-                                'stackDeleteComplete',
-                                cloudFormationStackExistParameters,
-                                (err: AWSError | null) => {
-                                    if (err) {
-                                        console.log(err);
-                                    } else {
-                                        console.log('Deletion completed');
-                                        this.create();
-                                        setTimeout(() => {
-                                            this.Cloud.waitFor(
-                                                'stackCreateComplete',
-                                                cloudFormationStackExistParameters,
-                                                (
-                                                    err: AWSError | null,
-                                                    data: CloudFormation.Types.DescribeStacksOutput,
-                                                ) => {
-                                                    if (err) {
-                                                        console.log(err);
-                                                    } else {
-                                                        if (data.Stacks?.length === 1) {
-                                                            console.log(
-                                                                'Finished creating ' +
-                                                                    this.stackName,
-                                                            );
-                                                        }
-                                                    }
-                                                },
-                                            );
-                                        }, 5000);
-                                    }
-                                },
-                            );
-                        }
-                    }
+                    throw stackNotFound;
                 }
-            },
-        );
+                throw error;
+            });
+
+            // Check stack was found
+            if (data.Stacks === undefined) {
+                throw Error(
+                    'Not able to find the given stack. Please check your configuration are correct.',
+                );
+            }
+
+            return data.Stacks[0].StackStatus as string;
+        } catch (error) {
+            if (error instanceof StackNotFoundException) {
+                const stackStatusRetrievingFailure = new StackStatusRetrievingException(
+                    this.stackName,
+                    'Failure to retrieve the status of the given stack.',
+                    `Error thrown in file ${getCurrentLine().file} on line ${
+                        getCurrentLine().line
+                    }`,
+                );
+
+                this.errorCollection.add(stackStatusRetrievingFailure);
+
+                throw stackStatusRetrievingFailure;
+            }
+            throw Error(error);
+        }
     }
+
+    public async createStack(): Promise<void> {
+        try {
+            const status = await this.getStackStatus();
+
+            if (status === 'CREATE_COMPLETE') {
+                const stackCreationFailure = new StackCreationException(
+                    this.stackName,
+                    'Failure to create a stack as it already exist in your AWS account.',
+                    `Error thrown in file ${getCurrentLine().file} on line ${
+                        getCurrentLine().line
+                    }`,
+                );
+
+                this.errorCollection.add(stackCreationFailure);
+
+                throw stackCreationFailure;
+            }
+
+            console.log(status);
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    // check if the stack exists if not
+    // create for the given stack
+
+    // if stack exists then
+    // delete the given stack
+
+    // if stack exists then
+    // redeploy stack by checking exists
 }
