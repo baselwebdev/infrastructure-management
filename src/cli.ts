45afa24430b48ad1ee3c1c5bd8f7feefa733aacf
Basel <basel@gmx.co.uk>
2021-02-23 23:25:44 +0000

    LP-160 Set process exit code
    On failure set the process exit code to 1.
    I can chain commands and check the results of this command
    before proceeding to the next command.

--- src/cli.ts
+++ src/cli.ts
@@ -108,6 +108,7 @@ switch (Yargs.argv.action) {
                 if (error instanceof Exception) {
                     console.log(Chalk.red(error.getMessage()));
                 }
+                process.exit(1);
             }
         })();
         break;
